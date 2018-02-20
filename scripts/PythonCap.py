55893fa96b91af755c0456262a3e1e3f037389b5
Zanz2 <zan.zagar@hotmail.com>
2018-02-20 16:04:25 +0000

    update 2

--- scripts/PythonCap.py
+++ scripts/PythonCap.py
@@ -1,11 +1,9 @@
 import cv2
-import time
 import numpy
 import sys
 def main():
     cam = cv2.VideoCapture(0)
     s, im = cam.read()
-    time.sleep(4)
     cam.release() 
     cv2.destroyAllWindows()
     cv2.imwrite("../images/snapshot_1.png",im)
@@ -14,7 +12,6 @@ def main():
 def throw_away():
     cam = cv2.VideoCapture(0)
     s, im = cam.read()
-    time.sleep(4)
     cam.release() 
     cv2.destroyAllWindows()
     cv2.imwrite("../images/snapshot_2.png",im)
--- scripts/camera_snapshot.sh
+++ scripts/camera_snapshot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm ../images/*
-sleep 5
+sudo rm ../images/*
+sleep 2
 sudo python PythonCap.py
-sleep 5
+sleep 2
 git add ../images/
 git commit -m "push img automated script"
 git push origin master
