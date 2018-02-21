90d267ae8ff5cea82489f583bd2361bc8262b6df
Zanz2 <zan.zagar@hotmail.com>
2018-02-21 07:19:05 +0000

    implementing serial comma

---
+++ scripts/serial_comms.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy
+import sys
+import imutils
+import time
+import serial
+def main():
+    ser = serial.Serial('/dev/tty-USB0',9600)
+
+if __name__ == '__main__':
+    main()
