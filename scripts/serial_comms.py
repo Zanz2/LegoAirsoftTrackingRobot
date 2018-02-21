420268e20292caa3fb04f9043cc9327821ec0608
Zanz2 <zan.zagar@hotmail.com>
2018-02-21 14:05:57 +0000

    servo camera movement

--- scripts/serial_comms.py
+++ scripts/serial_comms.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import cv2
 import numpy
 import sys
@@ -5,7 +6,24 @@ import imutils
 import time
 import serial
 def main():
-    ser = serial.Serial('/dev/tty-USB0',9600)
-
+    ser = serial.Serial('/dev/ttyUSB1',9600)
+    time.sleep(2)
+    list_arg = sys.argv
+    argument1 = list_arg[1]
+    argument2 = list_arg[2]
+    print(argument1)
+    print(argument2)
+    if argument1 == "write":
+        ser.write(argument2.encode())
+        time.sleep(3)
+        print("func write")
+    if argument1 == "read":
+        ser.write("180".encode())
+        while 1:
+            if ser.inWaiting():
+                print(ord((ser.read()).decode()))
+                break
+        print("func read")
+    ser.close()
 if __name__ == '__main__':
     main()
